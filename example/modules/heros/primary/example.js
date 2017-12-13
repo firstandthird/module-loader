a467ad1ab8078ac037c8fd25c3894697a90d8e23
Greg Allen <greg@firstandthird.com>
2017-12-13 10:40:44 -0800

    support multiple examples

--- example/modules/heros/primary/example.js
+++ example/modules/heros/primary/example.js
@@ -1,5 +1,11 @@
-module.exports = {
-  headline: 'example headline',
-  copy: 'example copy'
-};
+module.exports = [
+  {
+    headline: 'example headline',
+    copy: 'example copy'
+  },
+  {
+    headline: 'example headline 2',
+    copy: 'example copy 2'
+  }
+];
 
