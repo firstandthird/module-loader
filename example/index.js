654a4906925876ef9d61f15c4cada6ad6d39a865
Greg Allen <greg@firstandthird.com>
2017-12-14 09:31:35 -0800

    fixed example if only a single one

--- example/index.js
+++ example/index.js
@@ -22,5 +22,7 @@ const main = async function() {
   console.log('');
   console.log('EXAMPLE');
   console.log(modules.heros.primary.example('<div class="wrapper">', '</div>'));
+  console.log('EXAMPLE 2');
+  console.log(modules.heros.secondary.example('<div class="wrapper">', '</div>'));
 };
 main();
---
+++ example/modules/heros/secondary/example.js
@@ -0,0 +1,9 @@
+module.exports = {
+  content: {
+    headline: 'example headline',
+    copy: 'example copy'
+  },
+  user: {
+    name: 'bob'
+  }
+};
