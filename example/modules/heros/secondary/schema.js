b5e798fa6bfc34306ffdcffd585d3d3615b82aec
Alejandro Pérez Martín <aleperez92@gmail.com>
2018-05-17 15:20:20 +0100

    Add describe method, update example schema with more rules

--- example/modules/heros/secondary/schema.js
+++ example/modules/heros/secondary/schema.js
@@ -1,10 +1,15 @@
 const Joi = require('joi');
 module.exports = {
+  title: Joi.ref('content.headline'),
   content: Joi.object().keys({
-    headline: Joi.string().max(100),
-    copy: Joi.string()
+    headline: Joi.string().allow(''),
+    copy: Joi.string(),
+    number: Joi.number().max(100).valid([10, 12, 13])
   }),
-  user: Joi.object().keys({
-    name: Joi.string()
-  }).unknown()
+  user: Joi.alternatives([
+    Joi.object().keys({
+      name: Joi.string().default('Username').min(2)
+    }).unknown(),
+    Joi.string().required()
+  ])
 };
